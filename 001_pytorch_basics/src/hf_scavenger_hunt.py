e5e335671ee5c90cff8d5b0231a339d8d24f5f3a
ndrslmpk <andreas.lempik@gmail.com>
2025-02-07 23:53:31 +0100

    add experiments on nn's

--- 001_pytorch_basics/src/hf_scavenger_hunt.py
+++ 001_pytorch_basics/src/hf_scavenger_hunt.py
@@ -4,7 +4,28 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 my_tensor = torch.tensor([[0,1,2],[3,4,5],[6,7,8]])
 print(my_tensor)
 
+print(my_tensor.device.type)
 assert my_tensor.device.type in {"cuda", "cpu"}
 assert my_tensor.shape == (3,3)
 
 print("Success!")
+
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MyMLP(nn.Module):
+  def __init__(self):
+    super(MyMLP, self).__init__()
+    self.fc1 = nn.Linear(784,128)
+    self.fc2 = nn.Linear(128,10)
+    self.relu = nn.ReLU()
+    self.softmax = torch.softmax()
+
+  def forward(self, x):
+    x = self.fc1(x)
+    x = self.relu(x)
+    x = self.fc2(x)
+    x = self.softmax(x)
+
+    return x
--- 001_pytorch_basics/src/neural_networks.py
+++ 001_pytorch_basics/src/neural_networks.py
@@ -23,3 +23,31 @@ print(model)
 new_model = model.forward(torch.rand(10))
 print(new_model)
 
+
+# BY PyTorch official docs
+# NN serves image recognition
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class Net(nn.Module):
+  def __init__(self):
+    super(Net, self).__init__()
+
+    # convolution helps to make the relation between each pixel clearer  
+    self.conv1 = nn.Conv2d(1, 32, 3, 1)
+    self.conv2 = nn.Conv2d(32, 64, 3, 1)
+
+    self.dropout1 = nn.Dropout2d(0.25)
+    self.dropout2 = nn.Dropout2d(0.5)
+
+    # fc = fully connected layer
+    self.fc1 = nn.Linear(9216, 128)
+    # outputs 10 labels, as we use the MNIST dataset with all 0-9 digits
+    self.fc2 = nn.Linear(128,10)
+
+    my_nn = Net()
+    print(my_nn)
